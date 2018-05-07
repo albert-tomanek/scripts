cb067ecc9899aa2bec4f6bec80d6a33fd535c250
Albert Tomanek <electron826@gmail.com>
2018-05-07 19:53:36 +0200

    Fixed setup_swift.fish

--- setup_swift.fish
+++ setup_swift.fish
@@ -1,6 +1,6 @@
 # Appends commands that make Swift accessible to the shell init script.
 echo "export SWIFTENV_ROOT=\"$HOME/.swiftenv\"" >> ~/.config/fish/config.fish
-echo "set --export PATH $SWIFTENV_ROOT/bin $PATH" >> ~/.config/fish/config.fish
+echo "set --export PATH \$SWIFTENV_ROOT/bin \$PATH" >> ~/.config/fish/config.fish
 echo "eval (swiftenv init -)" >> ~/.config/fish/config.fish
 
 
